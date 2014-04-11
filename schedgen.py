f680d51e77c56bb61371ec190d1f4c072f832460
Sebastian Olsson <sebelino@hotmail.com>
2014-04-11 05:33:04 +0200

    Minimal HTML generator ready.

--- schedgen.py
+++ schedgen.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python2.7
 
 import sys,time,thread,os,argparse,select,csv,datetime
+from pprint import pprint
+import xml.dom.minidom as minidom
 
 """
 Simple schedule generator.
@@ -9,13 +11,23 @@ Simple schedule generator.
 
 __author__ = "Sebastian Olsson"
 
-def deltas(path):
+def prettyxml(xml):
+    doc = minidom.parseString(xml)
+    pretty = doc.toprettyxml(indent="  ",encoding="utf-8")
+    return str(pretty)
+
+def intervals(path):
     with open(path,'r') as csvfile:
         reader = csv.reader(csvfile)
         fmt = "%Y-%m-%dT%H:%M:%S"
         timeparse = lambda t: datetime.datetime.fromtimestamp(time.mktime(time.strptime(t,fmt)))
-        intervals = [(timeparse(stop)-timeparse(start),topic) for (start,stop,topic) in reader]
+        intervals = [(timeparse(start),timeparse(stop),topic) for (start,stop,topic) in reader]
         return intervals
+
+def myformat(inters):
+    year = 2014
+    month = 4
+    week = inters[0].isocalendar()[1]
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i","--input",type=str,metavar='file',default='schedule.csv',
@@ -31,12 +43,38 @@ working_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = "."
 path = os.path.join(data_dir,args.input)
 
-intervals = deltas(path)
-if args.subject:
-    intervals = [(t,s) for (t,s) in intervals if s == args.subject]
-datesum = datetime.timedelta(0)
-for (t,s) in intervals:
-    datesum += t
-output = [[datesum]] if args.sum else intervals
-for row in output:
-    print ' '.join([str(cell) for cell in row])
+#inters = myformat(intervals(path))
+#pprint(inters)
+
+record = {
+    'year': 2014, # Year of the first day of the week.
+    'week': 4,
+    'monday':    [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'tuesday':   [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'wednesday': [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'thursday':  [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'friday':    [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'saturday':  [((4,30),(10,0),'ml'),((10,0),(15,0),'os')],
+    'sunday':    [((4,30),(10,0),'ml'),((10,0),(15,0),'os')]
+}
+
+weekdays = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']
+year = record['year']
+week = record['week']
+html = '<table>'
+for day in weekdays:
+    dayrecord = record[day]
+    dayname = day[0].upper()+day[1:]
+    html += '<td><table height="100%">'
+    for ((ahrs,amin),(bhrs,bmin),s) in record[day]:
+        height = 100*(60*(bhrs-ahrs)+(bmin-amin))/(24*60)
+        trtag = '<tr height="%s%%">'% height
+        content = s
+        trend = '</tr>'
+        html += trtag+content+trend
+    html += '</table></td>'
+html += '</table>'
+
+print prettyxml(html)
+#print html
+
